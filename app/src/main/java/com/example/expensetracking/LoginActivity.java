4c78b71ba11b36ffb39b9db3e010c97485765f58
Bîrsan Dorin-Alexandru <alexodrino@gmail.com>
2025-01-02 06:27:28 +0200

    Change activity_main name to activity_login

--- app/src/main/java/com/example/expensetracking/LoginActivity.java
+++ app/src/main/java/com/example/expensetracking/LoginActivity.java
@@ -23,7 +23,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         emailInput = findViewById(R.id.emailInput);
         passwordInput = findViewById(R.id.passwordInput);
